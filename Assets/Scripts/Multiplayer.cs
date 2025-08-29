9a9e10f3dedf1abf61a1395c966c261a76b549f8
Arttu <Arttu.Myllari@edu.savonia.fi>
2025-08-30 02:09:55 +0300

    multiplayer toggle+ sigleplayer cam

---
+++ Assets/Scripts/Multiplayer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Multiplayer : MonoBehaviour
+{
+    public bool P2;
+    public bool loadedToggleState;
+
+    // Array of assets (GameObjects) that can be enabled/disabled
+    public GameObject[] assetsToEnable;
+    public GameObject[] assetsToDisable;
+
+    private void Start()
+    {
+        loadedToggleState = PlayerPrefs.GetInt("ToggleState", 0) == 1;
+        //loadedToggleState = true;
+        P2 = loadedToggleState;
+    }
+
+    void Update()
+    {
+
+        if (P2 == true)
+        {
+            foreach (GameObject asset in assetsToEnable)
+            {
+                asset.SetActive(true); // Enables each asset in the list
+            }
+            foreach (GameObject asset in assetsToDisable)
+            {
+            asset.SetActive(false); // Enables each asset in the list
+            }
+        }
+    }
+}
